f5de4386b8fcfd966aed4110178b72682d1176aa
bertolla <p.bertolla@noi.bz.it>
2021-03-18 15:46:56 +0100

    add inftrastructure for testing env

---
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    
+    environment {
+        PROJECT = "hello.net"
+        PROJECT_FOLDER = "data-collectors/${PROJECT}"
+        ARTIFACT_NAME = "dc-${PROJECT}"
+        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/hello.net'
+        DOCKER_TAG = "test-$BUILD_NUMBER"
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh """
+                    echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+                """
+            }
+        }
+        stage('Build') {
+            steps {
+                sh """
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
+                """
+            }
+        }
+        stage('Deploy') {
+            steps {
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+                        (cd infrastructure/ansible && ansible-playbook --limit=test deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                    """
+                }
+            }
+        }
+    }
+}
