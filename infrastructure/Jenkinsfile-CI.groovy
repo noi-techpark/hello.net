b167a1e5bcb912f257e2d9ec6d45afe4c612bda0
bertolla <p.bertolla@noi.bz.it>
2021-03-29 17:28:37 +0200

    first try for a ci pipeline

---
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/Dockerfile'
+        }
+    
+    stages {
+        stage('Configure') {
+            steps {
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'dotnet build'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'dotnet test /p:CollectCoverage=true'
+            }
+            
+        }
+    }
+}
