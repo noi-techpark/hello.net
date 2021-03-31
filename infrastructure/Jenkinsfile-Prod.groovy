7b86a347e9c50a0bb9136dc3b3d64c098347bfb9
bertolla <p.bertolla@noi.bz.it>
2021-03-31 11:20:17 +0200

    add production pipeline

---
+++ infrastructure/Jenkinsfile-Prod.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+    
+    environment {
+        PROJECT = "hello.net"
+        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/hello.net'
+        DOCKER_TAG = "prod-$BUILD_NUMBER"
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh """
+                    echo 'COMPOSE_PROJECT_NAME=${PROJECT}' > .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+                """
+            }
+        }
+        stage('Build') {
+            steps {
+                sh """
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
+                """
+            }
+        }
+        stage('Deploy') {
+            steps {
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        (cd infrastructure/ansible && ansible-galaxy install -f -r requirements.yml)
+                        (cd infrastructure/ansible && ansible-playbook --limit=prod deploy.yml --extra-vars "release_name=${BUILD_NUMBER} project_name=${PROJECT}")
+                    """
+                }
+            }
+        }
+    }
+}
